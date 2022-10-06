319c3166d6f15a89a7872c44174486dc5e46fe9c
xWynt3r <daniella.s.larsen@gmail.com>
2022-10-06 16:47:47 -0500

    Init Repo and Add Files

---
+++ skins/bottom-bar/dashboard.js
@@ -0,0 +1,69 @@
+﻿Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig, utils) {
+    //
+    // skinConfig - a copy of the skin configuration from config.json
+    // utils - an object containing several utility functions (see skin tutorial for more information)
+    //
+
+    // this function is called before everything else, 
+    // so you may perform any DOM or resource initializations / image preloading here
+
+    utils.preloadImages([
+        'images/bg-off.jpg', 'images/bg-on.jpg'
+    ]);
+
+    // return to menu by a click
+    $(document).add('body').on('click', function () {
+        window.history.back();
+    });
+}
+
+Funbit.Ets.Telemetry.Dashboard.prototype.filter = function (data, utils) {
+    //
+    // data - telemetry data JSON object
+    // utils - an object containing several utility functions (see skin tutorial for more information)
+    //
+
+    // This filter is used to change telemetry data 
+    // before it is displayed on the dashboard.
+    // You may convert km/h to mph, kilograms to tons, etc.
+
+    data.hasJob = data.trailer.attached;
+
+    // round truck speed
+    data.truck.speedRounded = Math.abs(data.truck.speed > 0
+        ? Math.floor(data.truck.speed)
+        : Math.round(data.truck.speed));
+
+    // other examples:
+
+    // convert kilometers per hour to miles per hour
+    data.truck.speedMph = data.truck.speed * 0.621371;
+    // convert kg to t
+    data.trailer.mass = (data.trailer.mass / 1000.0) + 't';
+    // calculate miles to go
+    data.navigation.milesToGo = data.navigation.estimatedDistance / 1609;
+    data.navigation.preEta = (data.navigation.milesToGo / data.truck.speedMph)/data.game.timeScale;
+    data.navigation.etaHours = Math.floor(data.navigation.preEta);
+    data.navigation.etaMinutes = Math.floor((data.navigation.preEta % 1) * 60);
+    // calculate wear
+    var wearSumPercent = data.truck.wearEngine * 100 +
+        data.truck.wearTransmission * 100 +
+        data.truck.wearCabin * 100 +
+        data.truck.wearChassis * 100 +
+        data.truck.wearWheels * 100;
+    wearSumPercent = Math.min(wearSumPercent, 100);
+    data.truck.wearSum = Math.round(wearSumPercent) + '%';
+    data.trailer.wear = Math.round(data.trailer.wear * 100) + '%';
+    // return changed data to the core for rendering
+    return data;
+};
+
+Funbit.Ets.Telemetry.Dashboard.prototype.render = function (data, utils) {
+    //
+    // data - same data object as in the filter function
+    // utils - an object containing several utility functions (see skin tutorial for more information)
+    //
+
+    // we don't have anything custom to render in this skin,
+    // but you may use jQuery here to update DOM or CSS
+}
